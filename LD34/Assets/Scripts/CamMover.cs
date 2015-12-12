e7ac34f9e03bc9b23ecdc3bd04048c337b1d2a5c
Jarl Larsson <jarl.larsson@gmail.com>
2015-12-12 09:58:07 +0100

    camera collision

--- LD34/Assets/Scripts/CamMover.cs
+++ LD34/Assets/Scripts/CamMover.cs
@@ -5,11 +5,15 @@ public class CamMover : MonoBehaviour
 {
     public float m_mouseMoveRotateMultiplier = 10.0f;
     public float m_mouseMovePlaceMultiplier = 10.0f;
+    public float m_keyMovePlaceMultiplier = 10.0f;
     public float m_mouseMoveZoomMultiplier = 10.0f;
     public Vector2 m_xRotateLims = new Vector2(300, 70);
     public bool isDragging = false;
     private float m_dragStatCoolDown = 0.4f;
     private float m_dragStatCoolDownTick = 0.0f;
+
+    public float m_rayCastOffset = 100.0f;
+    public float m_rayCastBound = 100.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +23,33 @@ public class CamMover : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+        InputHandling();
+        GroundCollideHandling();
+	}
+
+
+    void GroundCollideHandling()
+    {
+        RaycastHit rayHit;
+        if (Physics.Raycast(new Ray(transform.position + Vector3.up * m_rayCastOffset, Vector3.down), out rayHit))
+        {
+            if (transform.position.y < rayHit.point.y + m_rayCastBound)
+            {
+                Debug.DrawLine(transform.position + Vector3.up * m_rayCastOffset, rayHit.point, Color.green);
+                transform.position = new Vector3(transform.position.x, rayHit.point.y + m_rayCastBound, transform.position.z);
+            }
+            else
+            {
+                Debug.DrawLine(transform.position + Vector3.up * m_rayCastOffset, rayHit.point, Color.grey);
+            }
+        }
+    }
+
+
+    void InputHandling()
+    {
         Vector3 mouseInput = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse ScrollWheel"));
+        Vector3 keyInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
         // Rotate
         if (Input.GetAxis("RightClick") > 0.0f)
         {
@@ -46,9 +76,10 @@ public class CamMover : MonoBehaviour
         // Move
         if (Input.GetAxis("MiddleClick") > 0.0f)
         {
-            transform.position += transform.right*-mouseInput.x*m_mouseMovePlaceMultiplier + transform.up*-mouseInput.y*m_mouseMovePlaceMultiplier;
+            transform.position += transform.right * -mouseInput.x * m_mouseMovePlaceMultiplier + transform.up * -mouseInput.y * m_mouseMovePlaceMultiplier;
         }
+        transform.position += transform.right * keyInput.x * m_keyMovePlaceMultiplier + transform.forward * keyInput.y * m_keyMovePlaceMultiplier;
         // Zoom
         transform.position += transform.forward * mouseInput.z * m_mouseMoveZoomMultiplier;
-	}
+    }
 }
